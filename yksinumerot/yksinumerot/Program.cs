8a60ffe93d45496e26f457f51679ed407e954913
electricitas <142588614+electricitas@users.noreply.github.com>
2024-01-10 14:37:32 +0200

    Add files via upload

---
+++ yksinumerot/yksinumerot/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Anna nimi: ");
+string nimi = (Console.ReadLine());
+Console.WriteLine(nimi);
+
+Console.Write("1");
+Console.Write(" 11");
+Console.Write(" 101");
+Console.Write(" 1001");
