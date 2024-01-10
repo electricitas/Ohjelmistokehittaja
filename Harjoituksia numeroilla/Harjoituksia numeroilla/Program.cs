ab4cd0cf232b46fd57f9f127ffbd26530e245a0f
electricitas <142588614+electricitas@users.noreply.github.com>
2024-01-10 14:25:41 +0200

    Add files via upload

---
+++ Harjoituksia numeroilla/Harjoituksia numeroilla/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Anna eka numero: ");
+string eka = Console.ReadLine();
+Console.Write("Anna toka numero: ");
+string toka = Console.ReadLine();
+int tokaluku = Int32.Parse(toka);
+int summa = tokaluku + 3;
+Console.WriteLine("x = " + summa);
+//toka tehtävä
+Console.WriteLine("Toinen tehätävä");
+int erotus = tokaluku - 2;
+Console.WriteLine("x = " + erotus);
+//kolmas
+Console.WriteLine("Kolmas tehtävä");
+int kerto = tokaluku * 5;
+Console.WriteLine("x = " + kerto);
+//neljäs
+Console.WriteLine("Neljäs");
+int ekaluku = Int32.Parse(eka);
+int jako = ekaluku / tokaluku;
+Console.WriteLine("x = " + jako);
+//viides
+Console.WriteLine("Viides tehtävä");
+Console.Write("Anna eka numero: ");
+string ekaa = Console.ReadLine();
+Console.Write("Anna toka numero: ");
+string tokaa = Console.ReadLine();
+
