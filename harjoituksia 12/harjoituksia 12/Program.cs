f080fe77e571f656acdd2153416fd4bdc0cd0478
electricitas <142588614+electricitas@users.noreply.github.com>
2024-01-10 14:25:00 +0200

    Add files via upload

---
+++ harjoituksia 12/harjoituksia 12/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Syötä luku 10 tai suurempi: ");
+int inputNumber;
+
+while (!int.TryParse(Console.ReadLine(), out inputNumber) || inputNumber < 10)
+{
+    Console.WriteLine("Syötä luku 10 tai suurempi.");
+    Console.Write("Syötä luku uudelleen: ");
+}
+
+
+for (int i = 0; i <= 10; i++)
+{
+    for (int j = 0; j < 13; j++)
+    {
+        Console.Write($"{inputNumber}");
+
+        if (j == 12)
+        {
+            Console.WriteLine();
+        }
+        else if (i % 2 == 0)
+        {
+            Console.Write(" ");
+        }
+    }
+}
+
+Console.ReadLine();
