34234b41f163a4401a7ab7ced03d429776b3714e
electricitas <142588614+electricitas@users.noreply.github.com>
2024-01-10 14:26:20 +0200

    Add files via upload

---
+++ Ehdolliset määrittelyt/Ehdolliset määrittelyt/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Syötä ikäsi: ");
+int ika = int.Parse(Console.ReadLine());
+if (ika <15)
+{
+    Console.Write("Saat ajaa polkupyörää");
+}
+else if (ika < 18)
+{
+    Console.Write("Saat ajaa mopolla");
+}
+else
+{
+    Console.Write("Saat ajaa autolla");
+}
+
