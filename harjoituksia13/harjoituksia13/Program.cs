caa34e8594020db59e62502b2f194f883cbddfdd
electricitas <142588614+electricitas@users.noreply.github.com>
2024-01-15 22:49:36 +0200

    Add files via upload

---
+++ harjoituksia13/harjoituksia13/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Syötä sana: ");
+string sana = Console.ReadLine();
+
+if (sana.Length >= 2)
+{
+    char[] merkit = sana.ToCharArray();
+
+    char temp = merkit[0];
+    merkit[0] = merkit[sana.Length - 1];
+    merkit[sana.Length - 1] = temp;
+
+
+    string uusiSana = new string(merkit);
+    Console.WriteLine("Muutettu sana: " + uusiSana);
+}
+else
+{
+    Console.WriteLine("Syötä vähintään kaksi merkkiä pitkä sana.");
+}
+
+Console.ReadLine();
