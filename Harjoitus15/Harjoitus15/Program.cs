0897ca96fda231b2d262345ba3a4ba233d92ce38
electricitas <142588614+electricitas@users.noreply.github.com>
2024-01-15 22:51:44 +0200

    Add files via upload

---
+++ Harjoitus15/Harjoitus15/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Syötä lause");
+string lause = Console.ReadLine();
+
+string[] sanat = lause.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+string pisinSana = EtsiPisinSana(sanat);
+
+if (pisinSana != null)
+{
+    Console.WriteLine($"Pisin sana: {pisinSana}");
+}
+else
+{
+    Console.WriteLine("Lauseessa ei ollut sanoja.");
+}
+Console.ReadLine();
+
+static string EtsiPisinSana(string[] sanat)
+{
+    if (sanat.Length == 0)
+    { return null; }
+    string pisinSana = sanat[0];
+
+    foreach (string sana in sanat)
+    {
+        if (sanat.Length > pisinSana.Length)
+        {
+            pisinSana = sana;
+
+        }
+    }
+    return pisinSana;
+}
