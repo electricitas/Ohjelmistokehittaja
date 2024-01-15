938b10d75c8e535b6778da67ed56a75fc9f04895
electricitas <142588614+electricitas@users.noreply.github.com>
2024-01-15 22:50:54 +0200

    Add files via upload

---
+++ Harjoitus14/Harjoitus14/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Syötä ensimmäinen kokonaisluku: ");
+int luku1;
+
+while (!int.TryParse(Console.ReadLine(), out luku1))
+{
+    Console.WriteLine("Virheellinen syöte. Syötä kokonaisluku uudelleen: ");
+}
+
+Console.Write("Syötä toinen kokonaisluku: ");
+int luku2;
+
+while (!int.TryParse(Console.ReadLine(), out luku2))
+{
+    Console.WriteLine("Virheellinen syöte. Syötä kokonaisluku uudelleen: ");
+}
+
+
+if (luku1 > 0 && luku2 > 0)
+{
+    Console.WriteLine("Molemmat ovat positiivisia.");
+}
+else if (luku1 < 0 && luku2 < 0)
+{
+    Console.WriteLine("Molemmat ovat negatiivisia.");
+}
+else
+{
+    Console.WriteLine("Toinen on positiivinen ja toinen negatiivinen.");
+}
+
+Console.ReadLine();
