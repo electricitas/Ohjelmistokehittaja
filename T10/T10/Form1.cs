feea9300f7bda78996e237d672cf0c1a23ef633c
electricitas <142588614+electricitas@users.noreply.github.com>
2024-01-22 14:41:36 +0200

    Add files via upload

---
+++ T10/T10/Form1.cs
@@ -0,0 +1,54 @@
+namespace T10
+{
+    public partial class BMIForm : Form
+    {
+        public BMIForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LaskeBT_Click(object sender, EventArgs e)
+        {
+            double paino = 0, pituus = 0, painoindeksi;
+            paino = Convert.ToDouble(PainoTB.Text);
+            pituus = Convert.ToDouble(PituusTB.Text);
+            painoindeksi = Math.Round(paino / (pituus * pituus), 2);
+            if (painoindeksi < 18.5) 
+            {
+                VastausLB.Text = "Painoindeksisi on: " + painoindeksi;
+                VastausLB.ForeColor = Color.Blue;
+                KuvausLB.Text = "Alipaino";
+                KuvausLB.ForeColor = Color.Blue;
+                VastausLB.Visible= true;
+                KuvausLB.Visible= true;
+            }
+            else if (painoindeksi < 25)
+            {
+                VastausLB.Text = "Painoindeksisi on: " + painoindeksi;
+                VastausLB.ForeColor = Color.Green;
+                KuvausLB.Text = "Normaalipaino";
+                KuvausLB.ForeColor = Color.Green;
+                VastausLB.Visible = true;
+                KuvausLB.Visible = true;
+            }
+            else if (painoindeksi < 40)
+            {
+                VastausLB.Text = "Painoindeksisi on: " + painoindeksi;
+                VastausLB.ForeColor = Color.DarkOrange;
+                KuvausLB.Text = "Ylipaino";
+                KuvausLB.ForeColor = Color.DarkOrange;
+                VastausLB.Visible = true;
+                KuvausLB.Visible = true;
+            }
+            else
+            {
+                VastausLB.Text = "Painoindeksisi on: " + painoindeksi;
+                VastausLB.ForeColor = Color.Red;
+                KuvausLB.Text = "Huomattava ylipaino";
+                KuvausLB.ForeColor = Color.Red;
+                VastausLB.Visible = true;
+                KuvausLB.Visible = true;
+            }
+        }
+    }
+}
