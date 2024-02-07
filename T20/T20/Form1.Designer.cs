111aa74a15da6321d7c7f7beb39702bd0bd4493f
electricitas <142588614+electricitas@users.noreply.github.com>
2024-02-07 13:13:27 +0200

    Add files via upload

---
+++ T20/T20/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace T20
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.idTB = new System.Windows.Forms.TextBox();
+            this.enimiTB = new System.Windows.Forms.TextBox();
+            this.snimiTB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.puhTB = new System.Windows.Forms.TextBox();
+            this.emailTB = new System.Windows.Forms.TextBox();
+            this.opiskelijanroTB = new System.Windows.Forms.TextBox();
+            this.tyhjennaBT = new System.Windows.Forms.Button();
+            this.tallennaBT = new System.Windows.Forms.Button();
+            this.paivitaBT = new System.Windows.Forms.Button();
+            this.poistaBT = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ETUNIMI:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SUKUNIMI:";
+            // 
+            // idTB
+            // 
+            this.idTB.Location = new System.Drawing.Point(127, 17);
+            this.idTB.Name = "idTB";
+            this.idTB.Size = new System.Drawing.Size(234, 33);
+            this.idTB.TabIndex = 3;
+            // 
+            // enimiTB
+            // 
+            this.enimiTB.Location = new System.Drawing.Point(127, 75);
+            this.enimiTB.Name = "enimiTB";
+            this.enimiTB.Size = new System.Drawing.Size(234, 33);
+            this.enimiTB.TabIndex = 4;
+            // 
+            // snimiTB
+            // 
+            this.snimiTB.Location = new System.Drawing.Point(127, 130);
+            this.snimiTB.Name = "snimiTB";
+            this.snimiTB.Size = new System.Drawing.Size(234, 33);
+            this.snimiTB.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(397, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "PUHELIN:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(397, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "SÄHKÖPOSTI:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(397, 133);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "OPISKELIJANRO:";
+            // 
+            // puhTB
+            // 
+            this.puhTB.Location = new System.Drawing.Point(552, 14);
+            this.puhTB.Name = "puhTB";
+            this.puhTB.Size = new System.Drawing.Size(301, 33);
+            this.puhTB.TabIndex = 9;
+            // 
+            // emailTB
+            // 
+            this.emailTB.Location = new System.Drawing.Point(552, 72);
+            this.emailTB.Name = "emailTB";
+            this.emailTB.Size = new System.Drawing.Size(301, 33);
+            this.emailTB.TabIndex = 10;
+            // 
+            // opiskelijanroTB
+            // 
+            this.opiskelijanroTB.Location = new System.Drawing.Point(552, 125);
+            this.opiskelijanroTB.Name = "opiskelijanroTB";
+            this.opiskelijanroTB.Size = new System.Drawing.Size(301, 33);
+            this.opiskelijanroTB.TabIndex = 11;
+            // 
+            // tyhjennaBT
+            // 
+            this.tyhjennaBT.Location = new System.Drawing.Point(25, 190);
+            this.tyhjennaBT.Name = "tyhjennaBT";
+            this.tyhjennaBT.Size = new System.Drawing.Size(162, 42);
+            this.tyhjennaBT.TabIndex = 12;
+            this.tyhjennaBT.Text = "TYHJENNÄ";
+            this.tyhjennaBT.UseVisualStyleBackColor = true;
+            // 
+            // tallennaBT
+            // 
+            this.tallennaBT.Location = new System.Drawing.Point(212, 190);
+            this.tallennaBT.Name = "tallennaBT";
+            this.tallennaBT.Size = new System.Drawing.Size(162, 42);
+            this.tallennaBT.TabIndex = 13;
+            this.tallennaBT.Text = "TALLENNA";
+            this.tallennaBT.UseVisualStyleBackColor = true;
+            // 
+            // paivitaBT
+            // 
+            this.paivitaBT.Location = new System.Drawing.Point(397, 190);
+            this.paivitaBT.Name = "paivitaBT";
+            this.paivitaBT.Size = new System.Drawing.Size(162, 42);
+            this.paivitaBT.TabIndex = 14;
+            this.paivitaBT.Text = "PÄIVITÄ";
+            this.paivitaBT.UseVisualStyleBackColor = true;
+            // 
+            // poistaBT
+            // 
+            this.poistaBT.Location = new System.Drawing.Point(583, 190);
+            this.poistaBT.Name = "poistaBT";
+            this.poistaBT.Size = new System.Drawing.Size(162, 42);
+            this.poistaBT.TabIndex = 15;
+            this.poistaBT.Text = "POISTA";
+            this.poistaBT.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(37, 257);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(874, 427);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 696);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.poistaBT);
+            this.Controls.Add(this.paivitaBT);
+            this.Controls.Add(this.tallennaBT);
+            this.Controls.Add(this.tyhjennaBT);
+            this.Controls.Add(this.opiskelijanroTB);
+            this.Controls.Add(this.emailTB);
+            this.Controls.Add(this.puhTB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.snimiTB);
+            this.Controls.Add(this.enimiTB);
+            this.Controls.Add(this.idTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox idTB;
+        private TextBox enimiTB;
+        private TextBox snimiTB;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox puhTB;
+        private TextBox emailTB;
+        private TextBox opiskelijanroTB;
+        private Button tyhjennaBT;
+        private Button tallennaBT;
+        private Button paivitaBT;
+        private Button poistaBT;
+        private DataGridView dataGridView1;
+    }
+}
